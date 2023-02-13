5b2b1409dfd1e4caea61b0969a852568b9a55384
Georg Hackenberg <ghackenberg@gmail.com>
2023-02-13 18:44:59 +0100

    start working on p5 visualization

--- src/main/canvas/canvas.ts
+++ src/main/canvas/canvas.ts
@@ -1,58 +1,115 @@
+import * as P5 from "p5"
 import { BoxImpl, Component, CompositeImpl, CylinderImpl, Display, ImageImpl, Model, Primitive, SphereImpl, Vector } from "../index.js"
 
 export class Canvas {
-    private context: CanvasRenderingContext2D
-    constructor(private canvas: HTMLCanvasElement) {
-        this.context = canvas.getContext("2d")
+    private p: P5
+
+    constructor(private model: Model) {
+        this.p = new P5((p: P5) => {
+            p.preload = () => {
+                // empty
+            }
+            p.setup = () => {
+                p.createCanvas(100, 100, p.WEBGL)
+            }
+            p.draw = () => {
+                p.background(255)
+                this.drawModel()
+            }
+        })
     }
-    private renderComposite(position: Vector, orientation: Vector, scale: Vector, display: CompositeImpl) {
     
+    render() {
+        this.p.redraw()
     }
-    private renderBox(position: Vector, orientation: Vector, scale: Vector, display: BoxImpl) {
-    
+
+    private drawComposite(display: CompositeImpl) {
+        for (const child of display.children) {
+            this.drawDisplay(child)
+        }
     }
-    private renderSphere(position: Vector, orientation: Vector, scale: Vector, display: SphereImpl) {
-    
+    private drawBox(display: BoxImpl) {
+        this.p.box(display.width, display.height, display.length)
     }
-    private renderCylinder(position: Vector, orientation: Vector, scale: Vector, display: CylinderImpl) {
-    
+    private drawSphere(display: SphereImpl) {
+        this.p.sphere(display.radius)
     }
-    private renderPrimitive(position: Vector, orientation: Vector, scale: Vector, display: Primitive) {
+    private drawCylinder(display: CylinderImpl) {
+        this.p.cylinder(display.radius, display.height)
+    }
+    private drawPrimitive(display: Primitive) {
+        this.p.push()
+
+        this.p.color(display.color)
+
         if (display instanceof BoxImpl) {
-            this.renderBox(position, orientation, scale, display)
+            this.drawBox(display)
         } else if (display instanceof SphereImpl) {
-            this.renderSphere(position, orientation, scale, display)
+            this.drawSphere(display)
         } else if (display instanceof CylinderImpl) {
-            this.renderCylinder(position, orientation, scale, display)
+            this.drawCylinder(display)
         } else {
             throw "Primitive type not supported!"
         }
+
+        this.p.pop()
     }
-    private renderImage(position: Vector, orientation: Vector, scale: Vector, display: ImageImpl) {
-    
+    private drawImage(display: ImageImpl) {
+        throw "Image not implemented yet!"
     }
-    private renderComponent(component: Component<any, any>) {
+    private drawDisplay(display: Display) {
+        const position = display.position
+        const orientation = display.orientation
+        const scale = display.scale
+        
+        this.p.push()
+        
+        this.p.translate(position[0], position[1], position[2])
+        
+        this.p.rotateX(orientation[0])
+        this.p.rotateY(orientation[1])
+        this.p.rotateZ(orientation[2])
+        
+        this.p.scale(scale[0], scale[1], scale[2])
+
+        if (display instanceof CompositeImpl) {
+            this.drawComposite(display)
+        } else if (display instanceof Primitive) {
+            this.drawPrimitive(display)
+        } else if (display instanceof ImageImpl) {
+            this.drawImage(display)
+        } else {
+            throw "Display type not supported!"
+        }
+
+        this.p.pop()
+    }
+    private drawComponent(component: Component<any, any>) {
         const position = component.outputs.position as Vector
         const orientation = component.outputs.orientation as Vector
         const scale = component.outputs.scale as Vector
         const display = component.outputs.display as Display
-        if (display instanceof CompositeImpl) {
-            this.renderComposite(position, orientation, scale, display)
-        } else if (display instanceof Primitive) {
-            this.renderPrimitive(position, orientation, scale, display)
-        } else if (display instanceof ImageImpl) {
-            this.renderImage(position, orientation, scale, display)
-        } else {
-            throw "Display type not supported!"
-        }
+        
+        this.p.push()
+        
+        this.p.translate(position[0], position[1], position[2])
+        
+        this.p.rotateX(orientation[0])
+        this.p.rotateY(orientation[1])
+        this.p.rotateZ(orientation[2])
+        
+        this.p.scale(scale[0], scale[1], scale[2])
+
+        this.drawDisplay(display)
+
+        this.p.pop()
     }
-    render(model: Model) {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-        for (const component of model.staticComponents) {
-            this.renderComponent(component)
+    private drawModel() {
+        for (const c of this.model.staticComponents) {
+            this.drawComponent(c)
         }
-        for (const component of model.dynamicComponents) {
-            this.renderComponent(component)
+        for (const c of this.model.dynamicComponents) {
+            this.drawComponent(c)
         }
     }
 }
